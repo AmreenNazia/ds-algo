70fe57536fd440037e8c0a18753b3a6b68c5fa7d
AmreenNazia <156974279+AmreenNazia@users.noreply.github.com>
2024-02-02 11:49:10 -0600

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,7 +10,8 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class) 
 @CucumberOptions(features="src/test/resources/Feature/Tree.feature",glue = {"StepDefinition", "AppHooks"},
 monochrome = true,
-plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+	 	"html:target/htmlreport.html"}
 		 
 )
 public class TestRunner {
